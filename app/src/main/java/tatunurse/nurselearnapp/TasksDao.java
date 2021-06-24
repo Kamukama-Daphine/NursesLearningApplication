7bba9c47a3569856686041bcf40bf9420eec1c64
Kamukama-Daphine / <kamdmugisha@gmail.com>
2021-06-24 12:47:57 +0300

    second-commit

---
+++ app/src/main/java/tatunurse/nurselearnapp/TasksDao.java
@@ -0,0 +1,16 @@
+@Dao
+public interface TaskDao {
+
+    @Query("SELECT * FROM task")
+    List<Task> getAll();
+
+    @Insert
+    void insert(Task task);
+
+    @Delete
+    void delete(Task task);
+
+    @Update
+    void update(Task task);
+
+}
